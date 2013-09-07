e604fdb5d893aa92c5a95204b7e8eb17e0d2fa0f
Andrew Summers <apsummers@wisc.edu>
2013-09-07 16:11:56 -0500

    Making -p behave more like standard 'ps'

--- main.c
+++ main.c
@@ -2,9 +2,12 @@
 #include <stdio.h>
 
 #define OPT_SIZE 6
+#define NO_PID -1
+#define PID_ERR 0
 
 int main(int argc, char *argv[]) {
 
+    char *prog;
     int pid, scState, userTime, sysTime, virtMem, cmd;
     int optArray[OPT_SIZE];
 
@@ -13,13 +16,23 @@ int main(int argc, char *argv[]) {
         optArray[j] = 0;
     }
 
-    processOpts(argc, argv, optArray);
+    prog = argv[0];
+
+    switch (pid = processOpts(argc, argv, optArray)) {
+    case NO_PID:
+	break;
+    case PID_ERR:
+	fprintf(stderr, "%s: Invalid process id: \n ", prog);
+	break;
+    default:
+	printf("PID: %d\n", pid);
+	break;
+    }
 
     printf("optArray:\n");
 
-    int i;
-    for (i = 0; i < OPT_SIZE; i++) {
-        printf("%d", optArray[i]);
+    for (j = 0; j < OPT_SIZE; j++) {
+        printf("%d", optArray[j]);
     }
 
     printf("\n");
