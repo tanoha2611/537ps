ede6274020310cbf5fb005f501cace517cb736c9
Andrew Summers <apsummers@wisc.edu>
2013-09-12 10:18:55 -0500

    Lines no longer exceed 79 characters

--- optproc.h
+++ optproc.h
@@ -1,29 +1,32 @@
-/*********************************************************************
+/******************************************************************************
  * Authors: Andrew Summers apsummers@wisc.edu
  *          Hai Thach      thach@wisc.edu
  * Project: Programming Assignment 1, 537ps
  * File: optproc.h
- *********************************************************************/
+ *****************************************************************************/
 
 /*
  * Process command line arguments.  The following options are available:
  *
  * -p <pid>
- *     Display process information only for the process whose number is <pid>.  If this option is
- *     not present then display information for all processes of the current user.
+ *     Display process information only for the process whose number is <pid>.
+ *     If this option is not present then display information for all processes
+ *     of the current user.
  * -s
- *     Display the single character state information about the process.  If this option is not
- *     present, do not display this information.
+ *     Display the single character state information about the process.  If
+ *     this option is not present, do not display this information.
  * -U
- *     Display the amount of user time consumed by this process.  This defaults to TRUE, so if it
- *     is not present, then this information is displayed.
+ *     Display the amount of user time consumed by this process.  This
+ *     defaults to TRUE, so if it is not present, then this information is
+ *     displayed.
  * -S
  *     Display the amount of system time consumed by this process.
  * -v
- *     Display the amount of virtual memory currently being used (in pages) by this program.
+ *     Display the amount of virtual memory currently being used (in pages) by
+ *     this program.
  * -c
- *     Display the command line that started this program.  This defaults to TRUE, so if it is
- *     not present, then this information is displayed.
+ *     Display the command line that started this program.  This defaults to
+ *     TRUE, so if it is not present, then this information is displayed.
  *
  * Paramters:
  *   int - length of arguments array
